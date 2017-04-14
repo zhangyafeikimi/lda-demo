1125ffc446fde4c3d38b0b8527d1bbefc48c97a6
zhangyafeikimi <zhangyafeikimi@gmail.com>
2017-04-15 03:16:23 +0800

    improve run.sh

--- example/lightlda/run.sh
+++ example/lightlda/run.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+cd $(dirname $0)
 common_opt="-hp_opt 0 -K 3 -alpha 0.1 -beta 0.1 -total_iteration 200 -burnin_iteration 0 -log_likelihood_interval 10"
 
 ../../lda-train $common_opt -sampler lightlda -mh_step 2 ../train lightlda-mh2
